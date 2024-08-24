eaa9dc6a811449fb6fb2a4830f4061ab5a2e2c70
Gus Hogg-Blake <gus@gushogg-blake.com>
2024-08-24 09:34:18 +0100

    init

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import {resolve} from "path";
+import {defineConfig} from "vite";
+import {svelte} from "@sveltejs/vite-plugin-svelte";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [svelte()],
+	
+	resolve: {
+		alias: {
+			"$src": resolve(__dirname, "src"),
+			"$css": resolve(__dirname, "src/css"),
+			"$lib": resolve(__dirname, "src/lib"),
+			"$utils": resolve(__dirname, "src/utils"),
+			"$modules": resolve(__dirname, "src/modules"),
+			"$assets": resolve(__dirname, "src/assets"),
+			"$components": resolve(__dirname, "src/components"),
+		},
+	},
+});
